04a844b85c8a552b44355ebec6e1c9ed5878954d
Linda <lindajzhao@gmail.com>
2019-03-10 22:12:32 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { graphqlOperation } from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { withAuthenticator, S3Album } from 'aws-amplify-react';
+
+Amplify.configure(awsmobile);
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+      date
+    }
+  }
+}`
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`
 
 class App extends Component {
+  uploadFile = (evt) => {
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Amplify.Storage.put(name, file).then(() => {
+      alert('ok sent')
+      this.setState({ file: name });
+    })
+  }
+
+  todoMutation = async () => {
+    debugger
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+
+    const newEvent = await Amplify.API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newEvent));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await Amplify.API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+        <S3Album level="private" path='' />
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
